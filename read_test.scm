2f64e5e3e90887429044aaf1716069e7e0ab4c96
Antti Holvikari <anttih@gmail.com>
2011-10-18 20:43:05 +0300

    basic tokenizer and parser

--- read_test.scm
+++ read_test.scm
@@ -1,5 +1,13 @@
 (require-extension test)
 (include "read")
 
-(test "read number" (list 1) (read- "1"))
-(test "read two numbers" (list 1 2) (read- "1 2"))
+(test "tokenize number" (list '(number 1)) (tokenize "1"))
+(test "tokenize two numbers" (list '(number 1) '(number 2)) (tokenize "1 2"))
+(test "tokenize two longer numbers" (list '(number 11) '(number 21)) (tokenize "11 21"))
+
+(test "tokenize a list" (list '(open-paren) '(number 1) '(close-paren)) (tokenize "(1)"))
+
+(test "parse list with one number" (list '(1)) (parse '((open-paren) (number 1) (close-paren))))
+(test "parse list with two numbers"
+      (list '(1 2))
+      (parse '((open-paren) (number 1) (number 2) (close-paren))))
